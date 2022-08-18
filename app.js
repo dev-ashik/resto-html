d69b47df78497e7b3975a904c9ae3d8013684b87
MD ASHIK MAHMUD <ashik.swfu@outlook.com>
2022-08-18 16:41:55 +0800

    search from done

--- app.js
+++ app.js
@@ -5,3 +5,20 @@ menuBars.onclick = () => {
     menuBars.classList.toggle("fa-xmark");
     navbar.classList.toggle("active")
 }
+
+window.addEventListener("scroll", function(){
+    menuBars.classList.remove("fa-xmark");
+    navbar.classList.remove("active")
+});
+
+const searchIcon = document.querySelector("#search-icon");
+const closeBtn = document.querySelector("#close");
+const searchForm = document.querySelector("#search-form");
+
+searchIcon.onclick = () => {
+    searchForm.classList.toggle("active");
+}
+
+closeBtn.onclick = () => {
+    searchForm.classList.remove("active");
+}
