f64cace0a154be2a14d34bd5084bb386034324a5
MD ASHIK MAHMUD <ashik.swfu@outlook.com>
2022-08-18 19:31:04 +0800

    slider done

--- app.js
+++ app.js
@@ -22,3 +22,17 @@ searchIcon.onclick = () => {
 closeBtn.onclick = () => {
     searchForm.classList.remove("active");
 }
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop: true,
+  });
