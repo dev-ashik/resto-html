9d0ee015c8c0e164157b94d3aa63c68bf51de131
MD ASHIK MAHMUD <ashik.swfu@outlook.com>
2022-08-20 12:04:26 +0800

    order section done

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ closeBtn.onclick = () => {
 };
 
 
-// swiper
+// home swiper
 var swiper = new Swiper(".home-slider", {
   spaceBetween: 30,
   centeredSlides: true,
@@ -37,4 +37,29 @@ var swiper = new Swiper(".home-slider", {
     clickable: true,
   },
   loop: true,
+});
+
+// reveiw swiper
+var swiper = new Swiper(".review-slider", {
+  spaceBetween: 20,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    }
+  },
 });
